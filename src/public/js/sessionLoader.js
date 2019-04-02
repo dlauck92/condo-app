a7fad5b2338ca029286ad36a16e558dadb3824fd
dlauck92 <drewlauck92@gmail.com>
2019-04-02 19:16:17 -0400

    removing app

--- src/public/js/sessionLoader.js
+++
@@ -1,5 +0,0 @@
-define(['angular', 'angularjs/services/session', 'angularRoute', 'angularCookies', 'angularSanitize'], function (
-  angular
-) {
-  return angular.module('sessionLoader', ['trudesk.services.session', 'ngRoute', 'ngCookies', 'ngSanitize'])
-})
--- test/source/mailer.js
+++
@@ -1,1 +0,0 @@
-// var mailer    = require('../../src/mailer');
