3ba62f53660d633bfc0ff5fd4b752381a2f9eb26
Matthew McGee <mrmatthewmcgee@gmail.com>
2019-04-06 10:28:23 -0400

    first push

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+
+const home = props => (
+    <home className="home">
+        <p>I'm your home!</p>
+    </home>
+
+);
+
+export default home;
---
+++ src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './SideDrawer.css';
+
+const sideDrawer = props => {
+    let drawerClasses = 'side-drawer';
+    if (props.show) {
+        drawerClasses = 'side-drawer open';
+    }
+    return (
+        <nav className={drawerClasses}>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">Announcments</a>
+                </li>
+                <li>
+                    <a href="/Form">Work Orders</a>
+                </li>
+                <li>
+                    <a href="/">About</a>
+                </li>
+                <li>
+                    <a href="/">Contact</a>
+                </li>
+            </ul>
+        </nav>);
+};
+
+export default sideDrawer;
