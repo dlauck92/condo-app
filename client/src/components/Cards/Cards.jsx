83ea84786ba82745f773ba15131be6c5c82a14f7
dlauck92 <drewlauck92@gmail.com>
2019-04-17 21:05:27 -0400

    adding default component

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -3,6 +3,7 @@ import Card from './CardsUI';
 import img1 from '../img/img1.jpg';
 import img2 from '../img/img2.jpg';
 import img3 from '../img/img3.jpg';
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,12 +14,15 @@ class Cards extends Component {
                 <div className="row">
                     <div className="col-md-4">
                         <Card imgsrc={img1} title="Submit a Work Order"/>
+                        <Link to="WorkOrder" className="btn btn-outline-success">Submit</Link>
                     </div> 
                     <div className="col-md-4">
                         <Card imgsrc={img2} title="Contact Management"/>
+                        <Link to="Contact" className="btn btn-outline-success">Submit</Link>
                     </div>
                     <div className="col-md-4">
                         <Card imgsrc={img3} title="Just in Case"/>
+                        <Link to="Form" className="btn btn-outline-success">Submit</Link>
                     </div>
                 </div>
 
