47b6b8bf09cba3227acd7763ee901d554175aba6
Drew Lauck <43586307+dlauck92@users.noreply.github.com>
2019-04-17 21:09:00 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -96,8 +96,8 @@ class App extends Component {
               />
               <Route
                exact path='/Dashboard' 
-               component={Cards} />
-
+               component={Cards} 
+               />
               <Route
                 path='/LogIn'
                 render={(props) => <LogIn {...props} updateUser={this.updateUser} />}
@@ -106,8 +106,14 @@ class App extends Component {
                 path='/SignUp'
                 render={(props) => <SignUp {...props} />}
               />
-              {/* <Route exact path='/Dashboard' component={Dashboard} /> */}
-                {/* <Route exact path='/Form' component={Form} />  */}
+               <Route 
+                  exact path='/Dashboard'  
+                  component={Dashboard} 
+                />
+               <Route 
+                  exact path='/Form' 
+                   component={Form} 
+                />
               <SecretRoute
                 isAuthenticated={this.state.loggedIn}
                 exact path="/Dashboard"
