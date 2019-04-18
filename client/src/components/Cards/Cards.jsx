6c7fc6aa6fcf180e6350be28702f6eaf4342e66c
Drew Lauck <43586307+dlauck92@users.noreply.github.com>
2019-04-17 21:24:22 -0400

    Update Cards.jsx

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -4,8 +4,6 @@ import Card from './CardsUI';
 import img1 from '../img/img1.jpg';
 import img2 from '../img/img2.jpg';
 import img3 from '../img/img3.jpg';
-import { Link } from 'react-router-dom';
-
 
 
 class Cards extends Component {
