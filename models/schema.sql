c689f83a0e68f86834cb9f955b25ddb9acd6a4ca
dlauck92 <drewlauck92@gmail.com>
2019-04-28 15:08:35 -0400

    adding true to server.js

--- models/schema.sql
+++ models/schema.sql
@@ -1,18 +1,16 @@
-
-
 -- DROP DATABASE IF EXISTS chap_db;
+-- DROP DATABASE IF EXISTS sjxkkj5f7m74i8eb;
 
-
--- Creates the "chap_db" databas
+-- Creates the "chap_db" database
 -- CREATE DATABASE chap_db;
-
+-- CREATE DATABASE sjxkkj5f7m74i8eb;
 
 -- USE  chap_db;
+-- USE sjxkkj5f7m74i8eb;
 
 -- Values for testing purposes
 -- INSERT INTO  Users (name, username, about, email, password, last_login, status, admin, role, createdAt, updatedAt)
 -- VALUES ('Jane Doe', 'administrator', 'Manager', 'tlmclaughlin@gmail.com', 'password','2019-02-11 12:11:22.111','active','1','admin','2019-02-11 12:11:22.111','2019-02-11 12:11:22.111');
-
 
 --  INSERT INTO Posts (post_title, post_body, createdAt, updatedAt) 
 --  VALUES ('Board Meeting on 3-16-19', 'Meeting will be held at Drews unit #252 at 6:30pm. Topics are fixing loose siding and basement leak at unit #302', '2019-02-11 12:11:22', '2019-02-11 12:11:22');
